e8528d12d8fdcaae98a9796a52d3f975bb0dbe64
Luca2732 <96065475+Luca2732@users.noreply.github.com>
2024-06-18 09:52:48 +0200

    added the app interface via a localhost web app

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/gallery')
+def gallery():
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('gallery.html', images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return render_template('upload.html', filename=filename)
+
+if __name__ == "__main__":
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
